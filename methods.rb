dcbb45c7b3a2f513e2b2f432f1a0a23de9d0af9b
Andrew <aforest41@yahoo.com>
2023-01-09 08:42:15 -0800

    commit tree 94ce99228c91eb5bfb35cec32d06f96f10cc0809

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greetProgrammer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts name
+end
+
+def greetWithDefault name="programmer"
+    puts name
+end
+
+def add(num1,num2)
+    num1 + num2
+end
+
+def halve number
+    if number.class != 1.class
+        return nil
+    end
+
+    return number / 2
+end
